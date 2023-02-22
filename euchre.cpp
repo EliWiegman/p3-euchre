1cfa69e09e9fba20a3438d83f9f62fdd6fbd3837
Eli Wiegman <ebwiegman@gmail.com>
2023-02-21 22:50:50 -0500

    working on euchre.cpp

--- euchre.cpp
+++ euchre.cpp
@@ -9,21 +9,86 @@ using namespace std;
 void print_warning();
 
 class Game {
- public:
-  Game(/* ... */);
-  void play();
+    public:
+        Game(istream& is, bool shuffleDeck, int points, 
+        string p0_name, string p0_strat,
+        string p1_name, string p1_strat,
+        string p2_name, string p2_strat,
+        string p3_name, string p3_strat ) {
 
- private:
-  std::vector<Player*> players;
-  Pack pack;
-  // ...
+        };
 
-  void shuffle();
-  void deal(/* ... */);
-  void make_trump(/* ... */);
-  void play_hand(/* ... */);
-  // ...
+        void play() {
+            
+        };
+
+    private:
+        std::vector<Player*> players;
+        Suit trump;
+        Pack pack;
+        bool shuffled = false;
+
+        int player_order(Player* dealer, int distance) {
+            int position;
+
+            for (int i = 0; i < 4; i++) {
+                if (players[i] == dealer) {
+                    position = i;
+                }
+            }
+
+            return (position + distance) % 4;
+        };
+
+        void shuffle() {
+            if (shuffled) {
+                pack.shuffle();
+            } else {
+                pack.reset();
+            }
+        };
+
+        void deal(Player* dealer, int position) {
+            int pos;
+
+            for (int batch = 0; batch < 8; batch++) {
+                pos = player_order(dealer, batch);
+                switch (batch) {
+                    case 0: case 2: case 5: case 7:
+                        players[pos]->add_card(pack.deal_one());
+                        players[pos]->add_card(pack.deal_one());
+                        players[pos]->add_card(pack.deal_one());
+                        break;
+                    default:
+                        players[pos]->add_card(pack.deal_one());
+                        players[pos]->add_card(pack.deal_one());
+                        break;
+                }
+            }
+
+        };
+
+        void make_trump(Player* dealer) {
+            const Card upcard = pack.deal_one();
+            int pos;
+            for (int turn = 0; turn < 4; turn++) {
+                pos = player_order(dealer, turn);
+                if (players[pos]->make_trump(upcard, ((pos % 4) == 0), 1, trump)) {
+                    break;
+                }
+            }
+
+            if () {
+
+            };       
+        };
+
+        void play_hand() {
+
+        };
 };
+
+
 
 int main(int argc, char* argv[]) {
     string filename = argv[1];
@@ -34,11 +99,16 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    string shuffle = argv[2];
-    if (!((shuffle == "shuffle") || (shuffle == "noshuffle"))) {
+    string shuffle_str = argv[2];
+    bool shuffle;
+    if (shuffle_str == "shuffle") {
+        shuffle = true;
+    } else if (shuffle_str == "noshuffle") {
+        shuffle = false;
+    } else {
         print_warning();
         return -1;
-    };
+    }
 
     int points = atoi(argv[3]);
     if (!((points <= 100) && (points > 0))) {
@@ -74,6 +144,11 @@ int main(int argc, char* argv[]) {
         return -1;
     };
 
+    Game game = Game(input, shuffle, points, 
+                        p0_name, p0_strat, 
+                        p1_name, p1_strat, 
+                        p2_name, p2_strat,
+                        p3_name, p3_strat);
     return 0;
 }
 
