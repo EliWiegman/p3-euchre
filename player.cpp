3a8c88858864761d2057dccc6006c5cbfdc64d4f
Eli Wiegman <ebwiegman@gmail.com>
2023-03-01 23:05:16 -0500

    working on play_card

--- player.cpp
+++ player.cpp
@@ -142,17 +142,10 @@ class SimplePlayer : public Player {
             }
             if (suitCounter > 0)
             {
+                maxCard = Card(TWO, suit);
                 for (int i = 0; i < hand.size(); i++)
                 {
-                    if (hand[i].get_suit() == led_card.get_suit())
-                    {
-                        hand.erase(hand.begin() + i);
-                    }
-                }
-                maxCard = Card(TWO, led_card.get_suit(trump));
-                for (int i = 1; i < hand.size(); i++)
-                {
-                    if(Card_less(maxCard, hand[i], led_card, trump))
+                    if (Card_less(maxCard, hand[i], led_card, trump))
                     {
                         maxCard = hand[i];
                         index = i;
@@ -166,7 +159,7 @@ class SimplePlayer : public Player {
                 minCard = hand[0];
                 for (int i = 1; i < hand.size(); i++)
                 {
-                    if(Card_less(hand[i], minCard, trump))
+                    if (Card_less(hand[i], minCard, trump))
                     {
                         minCard = hand[i];
                         index = i;
